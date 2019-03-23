30549ce4c82d57650587a4f410a16a2f849da594
yousef_lab1 <shahadalmashaabi@Shahads-MacBook-Air-2.local>
2019-03-23 16:42:38 -0500

    first commit

--- calcNew/ViewController.swift
+++ calcNew/ViewController.swift
@@ -10,11 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var num1:Double=0
+    var num2:Double=0
+    var operation:Double=0
+    var performing=false
+    
+    @IBOutlet weak var TxtCalc: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func numberop( _ sender : UIButton){
+        if performing == true {
+            
+            TxtCalc.text = sender.titleLabel?.text
+            
+            num2 = Double(TxtCalc.text!)!
+            
+            performing = false
+            
+        }
+            
+        else {
+            
+            TxtCalc.text = TxtCalc.text! + (sender.titleLabel?.text)!
+            
+            num2 = Double(TxtCalc.text!)!
+            
+            
+        }
+    }
 
-
+    @IBAction func clear(_ sender: Any) {
+        TxtCalc.text=""
+        num1=0
+        num2=0
+        operation=0
+        performing=true
+    }
+    
+    @IBAction func buttons( _ sender : UIButton){
+        if TxtCalc.text != "" && sender.tag != 1{
+            num1 = Double(TxtCalc.text!)!
+            
+            operation = Double(sender.tag)
+            performing=true
+        }
+        else if sender.tag==1{
+            
+            if operation==2{
+                TxtCalc.text=String(num1+num2)
+            }
+            else if operation==3{
+                TxtCalc.text=String(num1-num2)
+            }
+            else if operation==4{
+                TxtCalc.text=String(num1*num2)
+            }
+            else if operation==5{
+                TxtCalc.text=String(num1/num2)
+            }
+            
+        }
+    }
+    
+    
 }
 
